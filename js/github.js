8d17538381fdbf2fed04c368dedd79a3b3c20e00
Angel Avgerinos <evaggelos.avgerinos@gmail.com>
2014-02-19 16:02:48 +0200

    Add Github avatar for Authors

--- js/github.js
+++ js/github.js
@@ -3,12 +3,16 @@ var Commit = Backbone.Model.extend({
     return {
       sha: this.get('sha'),
       author: this.get('commit').author.name,
-      message: this.get('commit').message
+      message: this.get('commit').message,
+      url: this.get('html_url'),
+      author_avatar: this.get('author').avatar_url
     };
   },
   detailsToHTML: function () {
     var details = this.details();
-    return "<tr><td>"+details.author+"</td><td>"+details.message+"</td><td>"+details.sha+"</td></tr>"
+    return "<tr><td><a href=" + details.url + ">" + details.sha + "</a></td><td>" +
+      "<img class='avatar' src=" + details.author_avatar + "/>" + details.author +
+      "</td><td>" + details.message+"</td></tr>"
   }
 });
 
